65f901a1f4767964ceceb7f4cfe8988587e5e7eb
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-20 13:51:13 +0200

    feat: import axios script

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Todo List JSON</title>
   <link rel="stylesheet" href="css/style.css">
+  <!-- Axios -->
+  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
 </head>
 <body>
 
