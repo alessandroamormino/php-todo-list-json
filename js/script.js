5653e4f72a0c8d10a8f7a4355f902ce00db56f01
alessandroamormino <amormino.alessandro97@gmail.com>
2023-04-20 14:57:48 +0200

    feat: add axios POST call and push new todo in JSON file

--- js/script.js
+++ js/script.js
@@ -10,13 +10,25 @@ createApp({
   methods: {
     getTodos() {
       axios.get('./server.php').then(response => {
-        console.log(response.data);
         this.todos = response.data;
       });
     },
+
     addTodo() {
-      this.todos.push(this.newTodo);
-      this.newTodo = '';
+      // dichiaro oggetto data 
+      let data = {
+        newTodo: '',
+      }
+      // valorizzo proprietà di data con il mio input
+      data.newTodo = this.newTodo;
+
+      // creo una chiamata POST axios che mi aggiunga nel file il nuovo todo inserito da input
+      axios.post('./server.php', data, { headers: { 'Content-Type': 'multipart/form-data' } }).then(response => {
+        // richiamo la funzione che fa la chiamata GET in modo da riscrivere in pagina il nuovo todo aggiunto
+        this.getTodos();
+      });
+      // azzero la variabile dell'input
+      this.newTodo = "";
     },
   },
   mounted() {
--- server.php
+++ server.php
@@ -1,14 +1,33 @@
 <?php 
-//  Reperisco il file JSON
-$todosJSON = file_get_contents('todos.json');
+// Controllo se sto ricevendo una chiamata POST
+if(isset($_POST['newTodo'])) {
+  //  Reperisco il file JSON
+  $todosJSON = file_get_contents('todos.json');
 
-// Trasformo il file JSON in un Array PHP
-$todos = json_decode($todosJSON);
+  // Trasformo il file JSON in un Array PHP
+  $todos = json_decode($todosJSON);
 
-// Faccio i miei calcoli
+  // Pusho il nuovo ToDo nell'array
+  $todos[] = $_POST['newTodo'];
 
-// Specifico che da ora in poi il file server.php sarà un JSON
-header('Content-type: application/json');
+  // Ritrasformo l'array in un file JSON
+  $newTodo = json_encode($todos);
 
-// Ritrasformo l'array in un file JSON
-echo json_encode($todos);
+  // Salvo il nuovo ToDo nel file JSON
+  file_put_contents('todos.json', $newTodo);
+
+} else {
+  //  Reperisco il file JSON
+  $todosJSON = file_get_contents('todos.json');
+  
+  // Trasformo il file JSON in un Array PHP
+  $todos = json_decode($todosJSON);
+  
+  // Faccio i miei calcoli
+  
+  // Specifico che da ora in poi il file server.php sarà un JSON
+  header('Content-type: application/json');
+  
+  // Ritrasformo l'array in un file JSON
+  echo json_encode($todos);
+}
